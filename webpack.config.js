4caa0431b3047d6fb7ea6674fe044f258f169f7b
Gabriel Johnson <leirbag75@gmail.com>
2022-04-19 10:40:22 -0700

    Set up Mocha with Webpack for testing

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,43 @@
 const path = require('path');
 
-module.exports = {
-  mode: 'development',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+module.exports = [
+  {
+    mode: 'development',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        test: /\.js$/,
+        include: [
+          path.join(__dirname, 'src')
+        ],
+        use: {
+          loader: 'babel-loader'
+        }
+      }]
+    }
   },
-  module: {
-    rules: [{
-      test: /\.js$/,
-      include: [
-        path.join(__dirname, 'src')
-      ],
-      use: {
-        loader: 'babel-loader'
-      }
-    }]
+  {
+    mode: 'development',
+    entry: path.resolve(__dirname, 'test/test.js'),
+    output: {
+      path: path.resolve(__dirname, 'test'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules : [{
+        test: /\.js$/,
+        include: [
+          path.join(__dirname, 'src'),
+          path.join(__dirname, 'test')
+        ],
+        exclude: /^bundle\.js$/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }]
+    }
   }
-}
+]
