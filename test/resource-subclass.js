e9651b7a1b95e823776bed973241c07a9f408172
Gabriel Johnson <leirbag75@gmail.com>
2022-07-19 16:28:29 -0700

    Create function to add Resource subclass tests to any given test suite

---
+++ test/resource-subclass.js
@@ -0,0 +1,52 @@
+import { describe, it } from 'mocha';
+import { act } from 'react-dom/test-utils';
+import sinon from 'sinon';
+import Resource from '../src/resource';
+import { strict as assert } from 'assert';
+import { describeComponent, assertCalledOnceWith  } from './react-test';
+
+export default function addResourceTests(
+  resourceClass,
+  description = 'as Resource'
+) {
+
+  describeComponent(description, reactTest => {
+
+    describe('componentDidMount', () => {
+
+      let backend, ref, url = 'https://api.com/resources/1';
+
+      beforeEach(() => {
+        ({ backend, ref } = reactTest);
+        backend.loadResource = sinon.fake();
+        reactTest.render(resourceClass, {url, backend});
+      });
+
+      it('should call loadResource on the backend', () => {
+        assertCalledOnceWith(
+          backend,
+          'loadResource',
+          url,
+          ref.current.readResource
+        );
+      });
+
+      it('should call read on all readers', () => {
+        let viewer = ref.current;
+        assert.equal(typeof viewer.readers, 'function');
+        function fakeReader() {
+          return {read: sinon.fake()};
+        }
+        let readers = [fakeReader(), fakeReader(), fakeReader()];
+        let resource = {};
+        sinon.replace(viewer, 'readers', sinon.fake.returns(readers));
+        viewer.readResource(resource);
+        for(let reader of readers)
+          assertCalledOnceWith(reader, 'read', resource, viewer);
+      });
+
+    });
+
+  });
+
+}
--- test/resource.js
+++ test/resource.js
@@ -4,42 +4,6 @@ import sinon from 'sinon';
 import Resource from '../src/resource';
 import { strict as assert } from 'assert';
 import { describeComponent, assertCalledOnceWith  } from './react-test';
+import addResourceTests from './resource-subclass';
 
-describeComponent('Resource', reactTest => {
-
-  describe('componentDidMount', () => {
-
-    let backend, ref, url = 'https://api.com/resources/1';
-
-    beforeEach(() => {
-      ({ backend, ref } = reactTest);
-      backend.loadResource = sinon.fake();
-      reactTest.render(Resource, {url, backend});
-    });
-
-    it('should call loadResource on the backend', () => {
-      assertCalledOnceWith(
-        backend,
-        'loadResource',
-        url,
-        ref.current.readResource
-      );
-    });
-
-    it('should call read on all readers', () => {
-      let viewer = ref.current;
-      assert.equal(typeof viewer.readers, 'function');
-      function fakeReader() {
-        return {read: sinon.fake()};
-      }
-      let readers = [fakeReader(), fakeReader(), fakeReader()];
-      let resource = {};
-      sinon.replace(viewer, 'readers', sinon.fake.returns(readers));
-      viewer.readResource(resource);
-      for(let reader of readers)
-        assertCalledOnceWith(reader, 'read', resource, viewer);
-    });
-
-  });
-
-});
+addResourceTests(Resource, 'Resource');
