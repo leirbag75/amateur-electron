89d16b37a787696b951cdac2572b712be85c6b07
Gabriel Johnson <leirbag75@gmail.com>
2022-07-26 12:17:12 -0700

    Remove unnecessary imports from Resource tests

--- test/resource.js
+++ test/resource.js
@@ -1,9 +1,4 @@
-import { describe, it } from 'mocha';
-import { act } from 'react-dom/test-utils';
-import sinon from 'sinon';
 import Resource from '../src/resource';
-import { strict as assert } from 'assert';
-import { describeComponent, assertCalledOnceWith  } from './test-helpers';
 import addResourceTests from './resource-subclass';
 
 addResourceTests(Resource, 'Resource');
