798076bf88ddacfda9851669d29a531ef68d83f2
Gabriel Johnson <leirbag75@gmail.com>
2022-08-16 13:10:33 -0700

    Actually instantiate the app on the HTML page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 // Webpack entry point
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './app';
+import ImageViewer from './image-viewer';
+import Backend from './backend';
+
+let root = createRoot(document.getElementById('root'));
+
+let backend = new Backend(window.backend);
+
+root.render(
+  <App url="entry" backend={backend} ref={backend.ref} />
+);
